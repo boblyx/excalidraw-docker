dc82703e38ad207941553e9d4dedf7e9c8f1f142
Bob Lee YX <128578144+boblyx@users.noreply.github.com>
2024-02-16 19:49:53 +0800

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone https://github.com/boblyx/excalidraw.git
+git clone https://github.com/boblyx/excalidraw-room
+git clone https://github.com/boblyx/excalidraw-storage-backend.git
+
+#docker compose up -d --build
